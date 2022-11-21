db6d89d01080f8b392a84f1aa294fb863528063f
nikki123-coder <60722664+nikki123-coder@users.noreply.github.com>
2022-11-21 15:41:14 +0530

    initial commit

---
+++ 1.java
@@ -0,0 +1,26 @@
+static void convert_Ternary(int n)
+    {
+
+        if (n == 0)
+            return;
+        int x = n % 3;
+        n /= 3;
+        if (x < 0)
+            n += 1;
+        convert_Ternary(n);
+        if (x < 0)
+            System.out.print( x + (3 * -1));
+        else
+            System.out.print( x);
+    }
+ static void convert(int decimal)
+    {
+        System.out.print("Ternary number of " +decimal +" is: ");
+
+
+        if (decimal != 0) {
+            convert_Ternary(decimal);
+        }
+        else
+            System.out.println("0" );
+    }
